75da0befdb49bc0e0293c20c0d73fde8ae32dcd3
Igor Vuk <parcijala@gmail.com>
2016-07-05 22:38:52 +0200

    Add the setup.py file

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""
+A simple Python 3 wrapper around The Setup's API.
+
+See:
+https://usesthis.com/api/
+https://github.com/ivuk/pyusesthis/
+"""
+
+from setuptools import setup
+
+setup(
+    name='pyusesthis',
+    version='0.0.1',
+    description="A wrapper around The Setup's API",
+    url='https://github.com/ivuk/pyusesthis',
+    author='Igor Vuk',
+    author_email='parcijala+pyusesthis@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        ],
+)
